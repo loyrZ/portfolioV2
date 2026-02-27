8466c29d011692c26446279dd971fd9bdfe8e49e
loyrZ <loyr22z@gmail.com>
2026-02-27 10:58:55 -0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'portfoliov2-production-ffc3.up.railway.app'
+    ]
+  }
 })
